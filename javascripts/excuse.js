f2ee550ceac2f58c95829ddc52493e360a7f712a
joe <santayjoseph@gmail.com>
2022-10-09 18:58:30 -0500

    finished project

--- javascripts/excuse.js
+++ javascripts/excuse.js
@@ -1,31 +1,61 @@
 let paragraph = document.getElementById("new-excuse")
 let category = document.getElementById("category")
 
-async function randomExcuse() {
+let specificParagraph = document.getElementById("new-specific-excuse")
+
+async function randomExcuse(url) {
     try {
-        const response = await fetch("https://excuser.herokuapp.com/v1/excuse")
+        const response = await fetch(url)
         const data = await response.json()
-        paragraph.innerHTML = data[0].excuse
-        category.innerHTML = data[0].category
+        category.innerHTML = "Category: "
+        category.innerHTML += data[0].category
+
+        paragraph.innerHTML = "Excuse: "
+        paragraph.innerHTML += data[0].excuse
+        
         
     } catch (e) {
         console.log("There was a problem fetching the breed list.")
     }
 }
 
-$(".excuse-button").click(function(){
-    specificExcuse()
+$(".random-excuse").click(function(){
+    randomExcuse("https://excuser.herokuapp.com/v1/excuse")
 })
 
-async function specificExcuse() {
+$(".specific-excuse").click(function(){
+    
+})
+
+// code for generating specific excuse goes down here
+$(".choose-college").click(function(){
+    specificExcuse("https://excuser.herokuapp.com/v1/excuse/college")
+})
+
+$(".choose-office").click(function(){
+    specificExcuse("https://excuser.herokuapp.com/v1/excuse/office")
+})
+
+$(".choose-party").click(function(){
+    specificExcuse("https://excuser.herokuapp.com/v1/excuse/party")
+})
+
+$(".choose-children").click(function(){
+    specificExcuse("https://excuser.herokuapp.com/v1/excuse/children")
+})
+
+$(".choose-family").click(function(){
+    specificExcuse("https://excuser.herokuapp.com/v1/excuse/family")
+})
+
+async function specificExcuse(url) {
     try {
-        const response = await fetch("https://excuser.herokuapp.com/v1/excuse/college")
+        const response = await fetch(url)
         const data = await response.json()
-        paragraph.innerHTML = data[0].excuse
-        category.innerHTML = data[0].category
+        specificParagraph.innerHTML = data[0].excuse
+        
         
     } catch (e) {
         console.log("There was a problem fetching the breed list.")
     }
 }
-
